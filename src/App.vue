be9f311364cae0ea318540b7f2d5cbd3e4633700
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-01 17:25:31 -0300

    trablhando com requisição http usando axios

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,18 @@ import axios from 'axios'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      pokemons: []
+    }
+  },
+  created() {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
+      console.log('Pegou lista')
+      this.pokemons = res.data.results
+      console.log(this.pokemons)
+    })
+  },
   components: {
     
   }
