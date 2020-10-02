d253939c528ae99e07d753b0a34aeda804838a2d
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-10-01 21:28:09 -0300

    adicionando busca em tempo real

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,57 @@
 <template>
   <div id="app">
     <div class="column is-half is-offset-one-quarter">
-    <img src="./assets/images/logos/download.png" alt="">
-      <div v-for="(poke, index) in pokemons" :key="index">
-      <!-- <h1>{{index+1}} {{poke.name}}</h1> -->
-      <Pokemon :name='poke.name' :url='poke.url' :num='index+1' />
-    </div>
+      <img src="./assets/images/logos/download.png" alt="" />
+      <h4 class="is-size-4">Pokedex</h4>
+      <input class="input is-rounded" type="text" placeholder="Buscar" v-model="busca">
+      <button class="button is-fullwidth is-link is-outlined btn-busca">Buscar</button>
+      <div v-for="(poke, index) in resultadoBusca" :key="index">
+        <!-- <h1>{{index+1}} {{poke.name}}</h1> -->
+        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import Pokemon from './components/Pokemon'
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
 
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
-      pokemons: []
-    }
+      pokemons: [],
+      busca: ''
+    };
   },
   created() {
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
-      console.log('Pegou lista')
-      this.pokemons = res.data.results
-      console.log(this.pokemons)
-    })
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        console.log("Pegou lista");
+        this.pokemons = res.data.results;
+        console.log(this.pokemons);
+      });
   },
   components: {
-    Pokemon
+    Pokemon,
+  },
+  computed: {
+    resultadoBusca(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons
+      }else {
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-
+.btn-busca {
+  margin-top: 10px;
+}
 </style>
 
